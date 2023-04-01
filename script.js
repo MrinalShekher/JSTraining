724649794d5d172c9a03f54ff99d7dc58ee69636
Mrinal Shekher Sharma <mrinalsharma101@gmail.com>
2023-04-01 23:04:18 +0530

    base project

---
+++ script.js
@@ -0,0 +1,11 @@
+function changeColor(){
+    console.log('changed');
+    let child = document.getElementById('one');
+    child.style.backgroundColor = "cyan";
+    if(true){
+        var a = 2;
+        let b = 3;
+        const c = 4;
+    }
+    console.log(a);
+}
